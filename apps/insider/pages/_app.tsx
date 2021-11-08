3d23c105fee0251a0e44761b5a41b454add31aca
anneau <hkobori@gmx.com>
2021-11-09 02:33:30 +0900

    update: delete default index

--- apps/insider/pages/_app.tsx
+++ apps/insider/pages/_app.tsx
@@ -6,14 +6,9 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to insider!</title>
+        <title>Insider Game | Asobiba</title>
       </Head>
       <div className="app">
-        <header className="flex">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src="/nx-logo-white.svg" alt="Nx logo" width="75" height="50" />
-          <h1>Welcome to insider!</h1>
-        </header>
         <main>
           <Component {...pageProps} />
         </main>
